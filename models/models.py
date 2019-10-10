17a62e9a508c7b8f99b8e04de3da8c5fc1fcd1b0
sarah <sweflorist@gmail.com>
2019-10-10 13:40:34 +0200

    flera info

--- models/models.py
+++ models/models.py
@@ -13,3 +13,22 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class sale_order(models.Model):
+    _inherit = "sale.order"
+
+    prio = fields.Boolean(string="Priority", default=False)
+
+
+    @api.one
+    def set_template(self, template_id):
+        self.sale_order_template_id = template_id
+
+        self._onchange_eval('sale_order_template_id', "1", {})
+        self._onchange_eval('partner_id', "1", {})
+
+    @api.one
+    def send_offer(self):
+        template = self.env.ref('sale.email_template_edi_sale')
+        template.write({'email_to': self.partner_id.email})
+        template.send_mail(self.id, force_send=True)
