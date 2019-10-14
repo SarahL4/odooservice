17d8f9bb7acff472a18967b00c70f6a31c80a0fe
sarah <sweflorist@gmail.com>
2019-10-14 22:28:09 +0200

    VG kvar

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -50,21 +50,34 @@ class ServiceMobile(http.Controller):
             logger.exception('kw %s' % post)
             order.note = post.get('note')
             order.prio = post.get('prio')
-
             order.order_line.product_uom_qty = post.get('qty')
-            # order.order_line.update_order_line(order.order_line.id)
-
             logger.exception('kw %s' % order.note)
+
+            new_task_params = {'date': datetime.datetime.now(),
+                               'employee_id': http.request.website.user_id.id,
+                               'name': post.get('name'),
+                               'unit_amount': float(post.get('hours')),
+                               'account_id': order.tasks_ids[0].project_id.analytic_account_id.id,
+                               'task_id': order.tasks_ids[0].id
+                               }
+
+            http.request.env['account.analytic.line'].create(new_task_params)
 
             return werkzeug.utils.redirect('/service/all/order/', 302)
         else:
             # sale_order_line_ids = order.order_line.search([('order_id', '=', order.id)])
+
             sale_order_line_ids = order.order_line.search([('order_id', '=', order.id),('product_uom.name', '=', 'Timme(ar)')])
+
+            task_objs = order.tasks_ids[0].project_id.analytic_account_id.line_ids.filtered(lambda r:r.task_id.sale_order_id.name==order.name)
             return http.request.render('service_mobile.view_order', {
                 'root': '/service/%s/order/' % order.id,
                 'partner_ids': http.request.env['res.partner'].search([('customer', '=', True)]),
                 'order': order,
                 'sale_order_line_ids': sale_order_line_ids,
+                'task_objs': task_objs,
+                'date': datetime.datetime.now().strftime('%Y-%m-%d'),
+                'employee': http.request.website.user_id.name,
                 'help': {'name': 'This is help string for name'},
                 'validation': {'name': 'Warning'},
                 'input_attrs': {},
